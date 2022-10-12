8d009f2e2f65886739fbf2a09ce32d30e694cbce
Rashay Daya <rashay.jcdaya@gmail.com>
2022-10-12 19:19:48 +0200

    Swap history

--- app/src/main/java/com/example/swapshop/SwapsFragment.java
+++ app/src/main/java/com/example/swapshop/SwapsFragment.java
@@ -1,15 +1,35 @@
 package com.example.swapshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListUpdateCallback;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SwapsFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private ImageAdapter mAdapter;
+    private DatabaseReference reference;
+    private List<Product> mUploads;
+    private List<String> productIDs;
 
     //empty constructor required for fragment
     public SwapsFragment() {
@@ -21,7 +41,86 @@ public class SwapsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_swaps, container, false);
+        View view = inflater.inflate(R.layout.fragment_swaps, container, false);
+
+        mRecyclerView = view.findViewById(R.id.recyclerview_CompletedSwaps);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        //Array lists
+        mUploads = new ArrayList<>();
+        productIDs = new ArrayList<>();
+
+        //reference to database
+        reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        //Finding items in database
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUploads.clear();
+                for(DataSnapshot postsnapshot: snapshot.getChildren()){
+
+                    String name = postsnapshot.child("name").getValue().toString();
+                    String description = postsnapshot.child("description").getValue().toString();
+                    String location = postsnapshot.child("location").getValue().toString();
+                    String img = postsnapshot.child("img").getValue().toString();
+                    String UID = postsnapshot.child("UID").getValue().toString();
+                    String reqProduct = postsnapshot.child("reqProduct").getValue().toString();
+                    String status = postsnapshot.child("status").getValue().toString();
+                    String category = postsnapshot.child("category").getValue().toString();
+
+                    Product objProduct = new Product(name,description,location,reqProduct,img,UID,status,category);
+
+                    //Adding product to list
+                    if(objProduct.checkSwapped() == true){
+                        productIDs.add(postsnapshot.getKey());
+                        mUploads.add(objProduct);
+                    }
+                }
+
+                //Getting image
+                mAdapter = new ImageAdapter(getActivity(),mUploads);
+
+                //Getting product
+                mAdapter.setOnItemClickListener(new ImageAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+                        //Toast.makeText(getContext(),"Swap click at: " + position,Toast.LENGTH_SHORT).show();
+                        Product currProduct = mUploads.get(position);
+                        String pID = productIDs.get(position);
+                        Intent intent = new Intent(getContext(), ViewProduct.class);
+                        intent.putExtra("Curr_Product", currProduct);
+                        intent.putExtra("Extra_ID",pID);
+                        startActivity(intent);
+
+                    }
+
+                    //Wishlist
+                    /*@Override
+                    public void onWishlistClick(int position) {
+                        Toast.makeText(getContext(),"Swap click at: " + position,Toast.LENGTH_SHORT).show();
+                    }
+
+                    //Swapped
+                    @Override
+                    public void onSwapped(int position) {
+                        Toast.makeText(getContext(),"Swap click at: " + position,Toast.LENGTH_SHORT).show();
+                    }*/
+                });
+
+                mRecyclerView.setAdapter(mAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(),"error",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return  view;
     }
 
 }
