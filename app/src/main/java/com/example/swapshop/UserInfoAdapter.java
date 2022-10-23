e2b535ad34b05ae3bbc01a4abc01fccceec3f857
Rashay Daya <rashay.jcdaya@gmail.com>
2022-10-23 13:25:23 +0200

    on click event

---
+++ app/src/main/java/com/example/swapshop/UserInfoAdapter.java
@@ -0,0 +1,86 @@
+package com.example.swapshop;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.UserInfoHolder> {
+    private Context mContext;
+    private List<Product> mUploads;
+    private OnItemClickListener mListener;
+
+    public UserInfoAdapter(Context context, List<Product> uploads){
+        mContext = context;
+        mUploads = uploads;
+    }
+
+
+    @NonNull
+    @Override
+    public UserInfoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(mContext).inflate(R.layout.userproduct_item,parent,false);
+        return new UserInfoHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserInfoHolder holder, int position) {
+        Product currProduct = mUploads.get(position);
+        holder.txtName.setText(currProduct.name);
+        Picasso.with(mContext)
+                .load(currProduct.img)
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(holder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class UserInfoHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public ImageView imageView;
+        public TextView txtName;
+
+        public UserInfoHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.UICV_product_image);
+            txtName = itemView.findViewById(R.id.UICV_product_title);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if(mListener != null){
+                int position = getAdapterPosition();
+                if(position != RecyclerView.NO_POSITION){
+                    mListener.onItemClick(position);
+                }
+            }
+        }
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        mListener = listener;
+    }
+
+
+}
--- app/src/main/java/com/example/swapshop/UserInfoFragment.java
+++ app/src/main/java/com/example/swapshop/UserInfoFragment.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,10 +23,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class UserInfoFragment extends Fragment {
     TextView txtUI_name;
     Button btnSignOut;
+    private RecyclerView mRecyclerView;
+    private UserInfoAdapter mAdapter;
+    private DatabaseReference reference;
+    private List<Product> mUploads;
+    private List<String> productIDs;
 
     public UserInfoFragment() {
         // Required empty public constructor
@@ -37,6 +49,7 @@ public class UserInfoFragment extends Fragment {
         //find view components
         txtUI_name = view.findViewById(R.id.txtUI_name);
         btnSignOut = view.findViewById(R.id.btnSignout);
+        mRecyclerView = view.findViewById(R.id.recyclerView_UserInfo);
 
         //initialise values
         String user = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -53,6 +66,65 @@ public class UserInfoFragment extends Fragment {
             }
         });
 
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2, RecyclerView.VERTICAL, false));
+        mUploads = new ArrayList<>();
+        productIDs = new ArrayList<>();
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                mUploads.clear();
+                for(DataSnapshot postsnapshot: snapshot.getChildren()){
+
+                    String name = postsnapshot.child("name").getValue().toString();
+                    String description = postsnapshot.child("description").getValue().toString();
+                    String location = postsnapshot.child("location").getValue().toString();
+                    String img = postsnapshot.child("img").getValue().toString();
+                    String UID = postsnapshot.child("UID").getValue().toString();
+                    String reqProduct = postsnapshot.child("reqProduct").getValue().toString();
+                    String status = postsnapshot.child("status").getValue().toString();
+                    String category = postsnapshot.child("category").getValue().toString();
+                    String swappedUID = postsnapshot.child("swappedUID").getValue().toString();
+
+                    Product objProduct = new Product(name,description,location,reqProduct,img,UID,status,category,swappedUID);
+
+
+                    //Adding product to list
+                    if(objProduct.UID.equals(user) ){
+                        productIDs.add(postsnapshot.getKey());
+                        mUploads.add(objProduct);
+                    }
+                }
+
+                mAdapter = new UserInfoAdapter(getActivity(),mUploads);
+                mAdapter.setOnItemClickListener(new UserInfoAdapter.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(int position) {
+                        Product currProduct = mUploads.get(position);
+                        String pID = productIDs.get(position);
+                        Intent intent = new Intent(getContext(), ViewProduct.class);
+                        intent.putExtra("Curr_Product", currProduct);
+                        intent.putExtra("Extra_ID",pID);
+                        intent.putExtra("Extra_login",false);
+                        startActivity(intent);
+                    }
+                });
+
+                mRecyclerView.setAdapter(mAdapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //on click events
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
