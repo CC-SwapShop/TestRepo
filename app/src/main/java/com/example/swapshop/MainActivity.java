0fd4d2e170a2cdb05741db2e0bcd5119743d904d
Jared747 <2304942@students.wits.ac.za>
2022-11-11 09:03:37 +0200

    tests

---
+++ app/src/main/java/com/example/swapshop/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.swapshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //using activity main xml
+        setContentView(R.layout.activity_main);
+    }
+}
